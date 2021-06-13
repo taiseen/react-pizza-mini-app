1d3ec19003943f88597f57095abcdbfc2e3c17d5
taiseen <taiseen.cse@gmail.com>
2021-06-13 11:37:43 +0600

    intro page ==> OK

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
+13-Jun-2021
 # Pizza React App (Just Font-End)
 
+# Technology Use
+React
+React Router 
+Tailwind CSS
+
+# Features
 * Shopping Cart
 * Place Order
 
-[Learning Video Link](https://www.youtube.com/watch?v=MfhZJcTOy1A)
-<br/>
-[Learning Resource Link](https://github.com/codersgyan/react-shopping-cart)
+* [Learning Video Link](https://www.youtube.com/watch?v=MfhZJcTOy1A)
+* [Learning Resource Link](https://github.com/codersgyan/react-shopping-cart)
+
+
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,12 +1,41 @@
 import { Link } from "react-router-dom"
 
 const Navigation = () => {
+
+    const cartStyle = {
+        backgroundColor: '#f59e0d',
+        display: 'flex',
+        borderRadius: 50,
+        padding: '6px 12px'
+    }
+
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/products'>Products</Link>
-            <Link to='/cart'>Carts</Link>
-        </div>
+        <nav className="container mx-auto flex items-center justify-between py-4">
+
+            <Link to='/'>
+                <img src="./img/logo.png" alt="logo" />
+            </Link>
+
+            <ul className="flex items-center">
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li className="ml-6">
+                    <Link to='/products'>Products</Link>
+                </li>
+                <li className="ml-6">
+                    <Link to='/cart'>
+                        <div style={cartStyle}>
+                            <span>10</span>
+                            <img className='ml-2' src="./img/cart.png" alt="cart" />
+                        </div>
+                    </Link>
+                </li>
+            </ul>
+
+
+
+        </nav>
     );
 };
 
